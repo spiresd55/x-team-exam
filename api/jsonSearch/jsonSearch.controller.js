8b03e9cf178e5fb8ce00938c1b575c905943bc32
dmspires <dmspires@us.ibm.com>
2016-06-10 13:00:16 -0500

    even more callback cleanup

--- api/jsonSearch/jsonSearch.controller.js
+++ api/jsonSearch/jsonSearch.controller.js
@@ -5,34 +5,33 @@ function searchJSONFiles(lineReader, searchTerms) {
         throw new Error('INVALID SEARCH TERMS');
     }
 
-    try {
-        jsonSearchService.loadJSONFilesIntoObjectArray(function (results) {
+    jsonSearchService.generateSearchTerms(searchTerms, searchTermsCallback);
 
-            jsonSearchService.generateSearchTerms(searchTerms, function (terms) {
 
-                console.log('Searching for the following terms `' + terms + '`');
+    function searchTermsCallback(terms) {
+        console.log('Searching for the following terms `' + terms + '`');
+        searchTerms = terms;
+        jsonSearchService.loadJSONFilesIntoObjectArray(jsonFilesCallback);
+    }
 
-                results = jsonSearchService.searchMapAndSort(terms, results);
-                console.log('########## FINAL RESULTS ##########');
+    function jsonFilesCallback(results) {
+        results = jsonSearchService.searchMapAndSort(searchTerms, results);
+        console.log('########## FINAL RESULTS ##########');
 
-                if(results.length === 0){
-                    console.log("No tags found matching your search terms")
-                }else {
-                    results.forEach(function (item) {
-                        console.log('%s: %d', item.term, item.count);
-                    });
-
-                    jsonSearchService.writeResults(results);
-                }
-
-                console.log();
-                lineReader.prompt();
+        if(results.length === 0){
+            console.log("No tags found matching your search terms")
+        }else {
+            results.forEach(function (item) {
+                console.log('%s: %d', item.term, item.count);
             });
 
-        });
-    }catch(error){
-        console.error('Something went wrong \n ' + error);
+            jsonSearchService.writeResults(results);
+        }
+
+        console.log();
+        lineReader.prompt();
     }
+
 }
 
 module.exports.searchJSONFiles = searchJSONFiles;
